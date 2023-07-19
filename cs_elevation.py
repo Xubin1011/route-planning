09863dbee9aee65a79f42f03183ad3a6cd9cc2ec
Xubin <127725480+Xubin1011@users.noreply.github.com>
2023-07-19 10:52:29 +0200

    update

--- cs_elevation.py
+++ cs_elevation.py
@@ -28,27 +28,27 @@ def get_elevation(api_key, latitude, longitude):
 
 #input api key, file path, the number of rows
 api_key = "5b3ce3597851110001cf624880a184fac65b416298dee8f52e43a0fe"
-file_path = "Ladesaeulenregister.xlsx"
+file_path = "cs_filtered_02.csv"
 rows_num = 5
 
 # read file
-df = pd.read_excel(file_path)
+df = pd.read_csv(file_path)
 
 #Get the latitude and longitude of the first rows_num rows,
-#get the altitude information and save it in the fourth column
+#get the altitude information and save it in new column
 for i, row in df.iloc[:rows_num].iterrows():
     df.at[i, "Elevation"] = get_elevation(api_key, row["Latitude"], row["Longitude"])
     time.sleep(1)  # Wait 1 second after each request
 
 # Swap the "Power" column with the "Elevation" column
-df = df[["Latitude", "Longitude", "Elevation", df.columns[2]]]
+df = df[["Latitude", "Longitude", "Elevation", "Max_power"]]
 
 # save to csv file
-output_file_path = "Ladesaeulenregister_01.csv"
+output_file_path = "cs_info.csv"
 df.iloc[:rows_num].to_csv(output_file_path, index=False)
 
 #Delete the first rows_num rows of the original table
 df = df.iloc[rows_num:]
-df.to_excel(file_path, index=False)
+df.to_csv(file_path, index=False)
 
 print("done")
---
+++ note.py
@@ -0,0 +1,12 @@
+# Date: 7/19/2023
+# Author: Xubin Zhang
+# Description: This file contains the implementation of...
+
+
+
+
+# 读取表格数据
+data = pd.read_excel('Ladesaeulenregister-processed.xlsx')
+
+# 输出 DataFrame 的列名
+print(data.columns)
