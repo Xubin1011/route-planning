9732d8f04265151ba697e4826ed7c7c26bf231b4
utlck <127725480+Xubin1011@users.noreply.github.com>
2023-09-15 11:21:49 +0200

    update

--- dqn.py
+++ dqn.py
@@ -18,7 +18,7 @@ import torch.optim as optim
 import torch.nn.functional as F
 
 import sys
-try_numbers = 21
+try_numbers = 25
 original_stdout = sys.stdout
 with open(f"output_{try_numbers:03d}.txt", 'w') as file:
     sys.stdout = file
@@ -35,7 +35,7 @@ with open(f"output_{try_numbers:03d}.txt", 'w') as file:
     GAMMA = 0.99  # GAMMA is the discount factor as mentioned in the previous section
     EPS_START = 0.9  # EPS_START is the starting value of epsilon
     EPS_END = 0.05  # EPS_END is the final value of epsilon
-    EPS_DECAY = 500  # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
+    EPS_DECAY = 2000  # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
     TAU = 0.005  # TAU is the update rate of the target network
     LR = 1e-4  # LR is the learning rate of the ``AdamW`` optimizer
     REPLAYBUFFER = 10000
