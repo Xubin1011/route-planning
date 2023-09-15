e79563d5576a56d759937156a4f6fa8c082d17eb
utlck <127725480+Xubin1011@users.noreply.github.com>
2023-09-15 09:45:42 +0200

    update

--- dqn.py
+++ dqn.py
@@ -35,7 +35,7 @@ with open(f"output_{try_numbers:03d}.txt", 'w') as file:
     GAMMA = 0.99  # GAMMA is the discount factor as mentioned in the previous section
     EPS_START = 0.9  # EPS_START is the starting value of epsilon
     EPS_END = 0.05  # EPS_END is the final value of epsilon
-    EPS_DECAY = 1000  # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
+    EPS_DECAY = 500  # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
     TAU = 0.005  # TAU is the update rate of the target network
     LR = 1e-4  # LR is the learning rate of the ``AdamW`` optimizer
     REPLAYBUFFER = 10000
