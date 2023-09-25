2d6390c2389a5b3404ee08896037e5ebad35e583
Xubin <127725480+Xubin1011@users.noreply.github.com>
2023-09-25 21:19:22 +0200

    update

--- dqn.bat
+++ dqn.bat
@@ -1,31 +1,50 @@
 @echo off
+setlocal enabledelayedexpansion
 
 set interpreter="E:\Program Files\miniconda3\envs\rp\python.exe"
 set script="G:\OneDrive\Thesis\Code\route-planning\dqn_n_actions.py"
 set try_number=38
 
-echo Running dqn_n_actions.py......
+echo Running dqn_n_actions.py...
 
-set "start_time=!time!"
-
-%interpreter% %script% %try_number%>> output_%try_number%.txt
-
-set "end_time=!time!"
-
-for /f "delims=:. tokens=1-4" %%a in ("!start_time!") do (
-    set /a "start_seconds=(((%%a * 60) + %%b) * 60) + %%c"
-)
-for /f "delims=:. tokens=1-4" %%a in ("!end_time!") do (
-    set /a "end_seconds=(((%%a * 60) + %%b) * 60) + %%c"
+for /f "tokens=1-3 delims=:" %%a in ("%TIME%") do (
+    set "start_hour=%%a"
+    set "start_minute=%%b"
+    set "start_second=%%c"
 )
 
-set /a "elapsed_seconds=end_seconds - start_seconds"
-set /a "hours=elapsed_seconds / 3600"
-set /a "minutes=(elapsed_seconds %% 3600) / 60"
-set /a "seconds=elapsed_seconds %% 60"
+%interpreter% %script% %try_number% >> output_%try_number%.txt
+
+for /f "tokens=1-3 delims=:" %%a in ("%TIME%") do (
+    set "end_hour=%%a"
+    set "end_minute=%%b"
+    set "end_second=%%c"
+)
+
+if !end_hour! lss !start_hour! (
+    set /a "hours=(24 + end_hour) - start_hour"
+) else (
+    set /a "hours=end_hour-start_hour"
+)
+
+if !end_minute! lss !start_minute! (
+    set /a "hours=hours-1"
+    set /a "minutes=(60 + end_minute) - start_minute"
+) else (
+    set /a "minutes=end_minute-start_minute"
+)
+
+if !end_second! lss !start_second! (
+    set /a "minutes=minutes-1"
+    set /a "seconds=(60 + end_second) - start_second"
+) else (
+    set /a "seconds=end_second-start_second"
+)
 
 echo Script completed in !hours!:!minutes!:!seconds!
 
-echo done！！！
+for /l %%i in (1,1,2) do (
+    powershell -c "(New-Object Media.SoundPlayer 'C:\Windows\Media\Alarm02.wav').PlaySync();"
+)
 
 pause
--- dqn_n_actions.py
+++ dqn_n_actions.py
@@ -28,7 +28,7 @@ else:
 #     sys.stdout = file
 
 if torch.cuda.is_available():
-    num_episodes = 15
+    num_episodes = 500
 else:
     num_episodes = 50
 
