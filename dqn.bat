fa95ddd3122aee361a7a9f38496d66e875e887cd
Xubin <127725480+Xubin1011@users.noreply.github.com>
2023-09-25 20:19:46 +0200

    update

---
+++ dqn.bat
@@ -0,0 +1,6 @@
+@echo off
+set interpreter="E:\Program Files\miniconda3\envs\rp\python.exe"
+set script="F:\OneDrive\Thesis\Code\route-planning\dqn_n_actions.py"
+set log="F:\OneDrive\Thesis\Code\route-planning\log.txt"
+
+%interpreter% %script% >> output_38.txt
